ebbe1a78cd0753810c2a97afeb1c5bd468e93083
Juanf Gallo <jufegare@gmail.com>
2019-10-23 20:33:47 -0500

    adding new features

---
+++ DigiSalud/Controllers/EncuestaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigiSalud.Controllers
+{
+    public class EncuestaController : Controller
+    {
+        // GET: Encuesta
+        public ActionResult Encuesta()
+        {
+            return View();
+        }
+    }
+}
